5cb770d98c831d27c8efb596cb87f055eef68974
Batul Abdulrahman <71075953+BatulAbdulrahman@users.noreply.github.com>
2022-08-17 22:57:14 +0200

    Add files via upload

---
+++ android/app/src/main/java/dev/jideguru/flutter_travel_concept/dev/jideguru/flutter_travel_concept/MainActivity.java
@@ -0,0 +1,11 @@
+package dev.jideguru.flutter_travel_concept;
+import io.flutter.embedding.android.FlutterActivity; 
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+
+public class MainActivity extends FlutterActivity {
+
+  }
+
+
+
